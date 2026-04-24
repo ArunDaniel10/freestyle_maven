3ab1a228bc88b5ac75245415ef4ffee606424938
ArunDaniel10 <arun11raja@gmail.com>
2026-04-24 11:00:13 +0530

    Update App.java

--- src/main/java/sample/App.java
+++ src/main/java/sample/App.java
@@ -5,6 +5,6 @@ package sample;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Helloooo World!");
+        System.out.println("Hell World!");
     }
 }
